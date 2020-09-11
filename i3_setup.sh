7f7ccac2e11974e8d9af333b5dba35cd5ca89338
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-09-11 18:34:09 +0530

    ttf-font-awesome

--- i3_setup.sh
+++ i3_setup.sh
@@ -1,5 +1,5 @@
 #i3 and lightdm
-sudo pacman -S i3 lightdm lightdm-gtk-greeter picom rofi
+sudo pacman -S i3 lightdm lightdm-gtk-greeter picom rofi ttf-font-awesome
 sudo enable lightdm
 #i3 config
 mkdir -p ~/.config
