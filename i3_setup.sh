be32dabaaeca451f906dd945c1b778120aecab0a
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-09-11 21:16:05 +0530

    i3lock install

--- i3_setup.sh
+++ i3_setup.sh
@@ -1,5 +1,5 @@
 #i3 and lightdm
-sudo pacman -S i3 lightdm lightdm-gtk-greeter picom rofi ttf-font-awesome
+sudo pacman -S i3 i3lock lightdm lightdm-gtk-greeter picom rofi ttf-font-awesome 
 sudo enable lightdm
 #i3 config
 mkdir -p ~/.config
