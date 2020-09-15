26784b2fc8e0d642fc56d3c825d04439d9e28e84
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-09-15 21:07:04 +0000

    zenity popup

--- i3_setup.sh
+++ i3_setup.sh
@@ -1,6 +1,6 @@
 echo "install graphics drivers before running this"
 #i3 and lightdm
-sudo pacman -S i3 lightdm lightdm-webkit2-greeter picom rofi ranger conky
+sudo pacman -S i3 lightdm lightdm-webkit2-greeter picom rofi ranger conky zenity
 sudo enable lightdm
 #i3 config
 mkdir -p ~/.config
