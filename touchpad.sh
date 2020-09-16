868379990c8b2c2024649d65e95273cc664977b3
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-09-16 17:42:24 +0000

    tocuhpad

--- touchpad.sh
+++ touchpad.sh
@@ -1,10 +1,2 @@
 sudo mkdir -p /etc/X11/xorg.conf.d
-echo "Section "InputClass"
-        Identifier "touchpad"
-        MatchIsTouchpad "on"
-        Driver "libinput"
-        Option "Tapping" "on"
-        Option "TappingButtonMap" "lrm"
-        Option "NaturalScrolling" "on"
-        Option "ScrollMethod" "twofinger"
-EndSection" | sudo tee /etc/X11/xorg.conf.d/90-touchpad.conf
+sudo cp touchpad_conf /etc/X11/xorg.conf.d/90-touchpad.conf
