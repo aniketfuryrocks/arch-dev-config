4208a856f9bc767e97f10e65a59018276b1da42f
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-09-17 17:29:45 +0530

    spotify build

--- gui_apps.sh
+++ gui_apps.sh
@@ -1,12 +1,12 @@
 #disk
 sudo pacman -S gnome-disk-utility gnome-keyring obs-studio firefox gpick atom
-#snap and flatpak
+#snap
 pamac build snapd
 sudo systemctl enable --now snapd.socket
 sudo systemctl start snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
-
-sudo pacman -S snap flatpak
+#flatpak
+sudo pacman -S flatpak
 #vscode
 sudo snap install code --classic
 #mailspring
@@ -14,7 +14,8 @@ sudo snap install mailspring --classic
 #discord
 sudo snap install discord --classic
 #spotify
-flatpak install spotify
+curl -sS https://download.spotify.com/debian/pubkey.gpg | gpg --import -
+pamac build spotify
 #woeusb
 pamac build woeusb-git
 #jetbrains toolbox
