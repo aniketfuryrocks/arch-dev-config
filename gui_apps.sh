2cdc953aebb622552d2c1b5f11f789584b8fbf9c
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-09-17 08:45:32 +0530

    snap and flatpak install

--- gui_apps.sh
+++ gui_apps.sh
@@ -1,5 +1,12 @@
 #disk
 sudo pacman -S gnome-disk-utility gnome-keyring obs-studio firefox gpick atom
+#snap and flatpak
+pamac build snapd
+sudo systemctl enable --now snapd.socket
+sudo systemctl start snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+sudo pacman -S snap flatpak
 #vscode
 sudo snap install code --classic
 #mailspring
